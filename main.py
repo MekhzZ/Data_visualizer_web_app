9e12bdc03247f5f658f68af6502a6cb9aa689bb5
Mekhma Tamang <166359437+MekhzZ@users.noreply.github.com>
2024-10-26 21:42:02 +0545

    Update main.py

--- main.py
+++ main.py
@@ -64,34 +64,7 @@ if selected_file:
             st.write("- Best for comparing different categories or groups.")
             plt.bar(df[x_axis],df[y_axis])
 
-        elif selected_plot == "Violin plot" :
-            st.write("")
-            st.write(selected_plot)
-            st.write("- Combines aspects of a box plot and a kernel density plot, showing the distribution of the data across different categories.")
-            st.write("- Useful for visualizing the distribution and density of the data, especially when comparing multiple categories")
-            sns.violinplot(x=df[x_axis],y=df[y_axis])
-
-        elif selected_plot == "Area plot" :
-            st.write("")
-            st.write(selected_plot)
-            st.write("- Similar to a line plot, but with the area below the line filled in, showing cumulative values over time.")
-            st.write("- Good for emphasizing the magnitude of change over time.")
-            plt.fill_between(df[x_axis],df[y_axis])
-
-        elif selected_plot == "Hexbin plot" :
-            st.write("")
-            st.write(selected_plot)
-            st.write("- Similar to a scatter plot but with hexagonal bins that aggregate data points, showing the density of points in each bin.")
-            st.write("- Effective for visualizing the density of large datasets and identifying clusters.")
-            plt.hexbin(x=df[x_axis], y=df[y_axis], gridsize=30, cmap='Blues')
-            plt.colorbar()
-
-        elif selected_plot == "Density plot" :
-            st.write("")
-            st.write(selected_plot)
-            st.write("- A smoothed version of a histogram, showing the probability density of the data using a continuous curve.")
-            st.write("- Ideal for understanding the distribution of a dataset.")
-            sns.kdeplot(x=df[x_axis],y=df[y_axis])
+)
 
 
 
